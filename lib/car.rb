c0fed9af9ffac53b32bfaeecf8731a9b489f2358
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-18 15:15:03 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,18 @@
    require_relative "./vehicle.rb"
 class Car 
   
-  attr_accessor :wheel_size, :whee
+  attr_accessor :wheel_size, :wheel_number
+  
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+  
+  def go 
+    "vrrrrrroooom!"
+  end
+  
+  def fill_up_tank
+    "filling up!"
+  end
 end
